71c92d7c847c1b9ab1e51820d8093b7aa3cc09d9
Vincent Genevès <vincent@geneves.fr>
2023-04-08 17:26:47 +0200

    Update debug config

--- boards/arm/b500e/board.cmake
+++ boards/arm/b500e/board.cmake
@@ -2,4 +2,5 @@
 
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
